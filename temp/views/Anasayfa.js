b2a1db4f3870a52d639fe21646b0a827745a5199
Osman Aşar <osmanasar933@gmail.com>
2020-03-21 22:02:46 +0300

    react-native-linear-gradient
    1) npm install --save react-native-linear-gradient
    2)npm yeniden aç
    
    Örnek Kod:
    <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
    </LinearGradient>

--- temp/views/Anasayfa.js
+++ temp/views/Anasayfa.js
@@ -4,6 +4,7 @@ import {Container, Content, Text} from 'native-base';
 import axios from "axios";
 import HTML from 'react-native-render-html';
 import FastImage from "react-native-fast-image";
+import LinearGradient from 'react-native-linear-gradient';
 const htmlContent = `
     <h1>This HTML snippet is now rendered with native components !</h1>
     <h2>Enjoy a webview-free and blazing fast application</h2>
@@ -24,6 +25,7 @@ export default class Anasayfa extends React.Component {
 	render() {
 		return (
 			<Container style={{flex:1,backgroundColor:'#f54136'}}>
+				<LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
 				<Content padder>
 					<Text>Anasayfa</Text>
 					<FastImage
@@ -33,6 +35,7 @@ export default class Anasayfa extends React.Component {
 			        />
 					<HTML html={htmlContent} imagesMaxWidth={Dimensions.get('window').width} />
 				</Content>
+				</LinearGradient>
 			</Container>
 		);
 	}
@@ -43,5 +46,8 @@ const styles = {
     width: 200,
     height: 200,
   },
+  linearGradient:{
+  	flex:1
+  }
  
 };
