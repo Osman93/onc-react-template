3f25818b6994f84209682b15edf02e2cf0263a05
Osman Aşar <osmanasar933@gmail.com>
2020-03-21 21:54:01 +0300

    react-native-fast-image kurulumu
    1)npm install --save react-native-fast-image
    Örnek kod:
    <FastImage
             			 source={require('../assets/oncLogo.png')}
    			          resizeMode="contain"
    			          style={[styles.icon]}
    			        />

--- temp/views/Anasayfa.js
+++ temp/views/Anasayfa.js
@@ -3,6 +3,7 @@ import { Dimensions } from "react-native";
 import {Container, Content, Text} from 'native-base';
 import axios from "axios";
 import HTML from 'react-native-render-html';
+import FastImage from "react-native-fast-image";
 const htmlContent = `
     <h1>This HTML snippet is now rendered with native components !</h1>
     <h2>Enjoy a webview-free and blazing fast application</h2>
@@ -22,12 +23,25 @@ export default class Anasayfa extends React.Component {
 	}
 	render() {
 		return (
-			<Container>
+			<Container style={{flex:1,backgroundColor:'#f54136'}}>
 				<Content padder>
 					<Text>Anasayfa</Text>
+					<FastImage
+         			 source={require('../assets/oncLogo.png')}
+			          resizeMode="contain"
+			          style={[styles.icon]}
+			        />
 					<HTML html={htmlContent} imagesMaxWidth={Dimensions.get('window').width} />
 				</Content>
 			</Container>
 		);
 	}
 }
+const styles = {
+ 
+  icon: {
+    width: 200,
+    height: 200,
+  },
+ 
+};
