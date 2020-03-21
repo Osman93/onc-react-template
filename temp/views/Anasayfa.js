be9a1d1a1b48e0cbb517ea4542ab5776c7ee2e32
Osman Aşar <osmanasar933@gmail.com>
2020-03-21 21:19:33 +0300

    axios kurulumu ve anasayfada gösterilmesi
    1) npm install --save axios
    
    Örnek kod:
    axios.get('http://on-csoft.com/akis')
    		.then((res) => {
    			console.log(res.data);
    		})
    		.catch((err) => {
    			console.log(err.response);
    		})

--- temp/views/Anasayfa.js
+++ temp/views/Anasayfa.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import {Container, Content, Text} from 'native-base';
-
+import axios from "axios";
 export default class Anasayfa extends React.Component {
+	componentWillMount(){
+		axios.get('http://on-csoft.com/akis')
+		.then((res) => {
+			console.log(res.data);
+		})
+		.catch((err) => {
+			console.log(err.response);
+		})
+	}
 	render() {
 		return (
 			<Container>
