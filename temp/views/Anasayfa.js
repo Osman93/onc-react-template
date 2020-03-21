f69ff7e57b9181ae18beab9be576f789cdf5f5e7
Osman Aşar <osmanasar933@gmail.com>
2020-03-21 21:38:00 +0300

    react-native-render-html ve react-native-webview kurma
    1) npm install --save react-native-render-html
    2)npm install --save react-native-webview
    3)react-native link react-native-webview
    4)Android studio aç kapa npm aç kapa
    
    Örnek kod:
    <HTML html={htmlContent} imagesMaxWidth={Dimensions.get('window').width} />

--- temp/views/Anasayfa.js
+++ temp/views/Anasayfa.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Dimensions } from "react-native";
 import {Container, Content, Text} from 'native-base';
 import axios from "axios";
+import HTML from 'react-native-render-html';
+const htmlContent = `
+    <h1>This HTML snippet is now rendered with native components !</h1>
+    <h2>Enjoy a webview-free and blazing fast application</h2>
+    <img src="https://i.imgur.com/dHLmxfO.jpg?2" />
+    <em style="textAlign: center;">Look at how happy this native cat is</em>
+`;
 export default class Anasayfa extends React.Component {
+
 	componentWillMount(){
 		axios.get('http://on-csoft.com/akis')
 		.then((res) => {
@@ -16,6 +25,7 @@ export default class Anasayfa extends React.Component {
 			<Container>
 				<Content padder>
 					<Text>Anasayfa</Text>
+					<HTML html={htmlContent} imagesMaxWidth={Dimensions.get('window').width} />
 				</Content>
 			</Container>
 		);
