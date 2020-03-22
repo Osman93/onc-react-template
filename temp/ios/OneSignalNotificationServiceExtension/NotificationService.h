dcc93f970173651951e79b09042bbc54f4ceeeb9
Osman Aşar <osmanasar933@gmail.com>
2020-03-22 15:30:25 +0300

    ios kurulumlar
    Son olarak yeni yükleyeceklerin yapması gerekenler
    1) npm install
    2) cd ios && pod install
    ardından kullanabilirler

---
+++ temp/ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by ONC on 22.03.2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
